123a2f2327ba2e087dfe01e799b10882bd8460b1
TwKnD <43603021+TwKnD@users.noreply.github.com>
2020-05-16 07:12:49 +1000

    Test check for special char at word end

--- test.py
+++ test.py
@@ -18,9 +18,21 @@ def c1r(string):
 
 
 # check for special char at end of word.
+def e1(string):
+    l = []
+    for w in string.split():
+        print(w)
+        if w[-1].isalpha():
+            l.append(w[1:] + w[0] + 'ay')
+        else:
+            l.append(w[1:-1] + w[0] + 'ay' + w[-1])
+
+    return ' '.join(l)
 
 
+# Combine both checks
 
-print(c1r('test test !'))
-print(c1r('test test!'))
-print(c1r('test test !!!'))
+
+print(e1('test test !'))
+print(e1('test test!'))
+print(e1('test test !!!'))
