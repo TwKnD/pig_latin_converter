e7c362f92dfa18f71c67bc464a811c2f6ecf2169
TwKnD <43603021+TwKnD@users.noreply.github.com>
2020-05-16 07:52:52 +1000

    Clean up for pep8

--- test.py
+++ test.py
@@ -8,58 +8,62 @@ def to_en(string):
     # pig latin to english
     pass
 
+
 # check  for special char at end of str after space.
 def e1(string):
-    l = []
+    l1 = []
     for w in string.split():
         print(w)
         if w.isalpha():
-            l.append(w[1:] + w[0] + 'ay')
-            print(l)
+            l1.append(w[1:] + w[0] + 'ay')
+            print(l1)
         else:
-            l.append(w)
-            print(l)
+            l1.append(w)
+            print(l1)
 
-    return ' '.join(l)
+    return ' '.join(l1)
 
 
 def e1r(string):
-    return ' '.join([w[1:]+w[0]+'ay' if w.isalpha() else w for w in string.split()])
+    return ' '.join([w[1:]+w[0]+'ay' if w.isalpha()
+                    else w for w in string.split()])
 
 
 # check for special char at end of word.
 def w1(string):
-    l = []
+    l1 = []
     for w in string.split():
         print(w)
         if w[-1].isalpha():
-            l.append(w[1:] + w[0] + 'ay')
+            l1.append(w[1:] + w[0] + 'ay')
         else:
-            l.append(w[1:-1] + w[0] + 'ay' + w[-1])
+            l1.append(w[1:-1] + w[0] + 'ay' + w[-1])
 
-    return ' '.join(l)
+    return ' '.join(l1)
 
 
 # Combine both checks
 def b1(string):
     # english to Pig Latin
-    l = []
+    # TODO: Convert to lower.
+    l1 = []
     for w in string.split():
         if not w[-1].isalpha() and w[:-1].isalpha():
-            l.append(w[1:-1] + w[0] + 'ay' + w[-1])
+            l1.append(w[1:-1] + w[0] + 'ay' + w[-1])
         elif not w.isalpha():
-            l.append(w)
+            l1.append(w)
         else:
-            l.append(w[1:] + w[0] + 'ay')
+            l1.append(w[1:] + w[0] + 'ay')
 
-    return ' '.join(l)
+    return ' '.join(l1)
 
 
 def b2(string):
     # Pig Latin to english
     pass
-    
+
 
 print(b1('test test !'))
 print(b1('test test!'))
 print(b1('test test !!!'))
+print(b1('This line, Is a test line.'))
